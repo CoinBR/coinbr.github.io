e9225a4914842414506858c235646f6761bb8b28
Pedro Duarte <coinbr@gmail.com>
2018-06-26 16:42:39 -0300

    add tmp marilia project site

---
+++ tmp/pedro/scripts/processElementData.js
@@ -0,0 +1,83 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uw,_6olHEXWsEeiqgqlHHY5nngActivity"]="./../../CoinsWay/deliveryprocesses/resources/Aprovar Implementações_75A19CB4_64313b01_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uwActivity"]="./../../CoinsWay/deliveryprocesses/resources/Processo_48546145_48546145_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uw,_Sr6ZsXWsEeiqgqlHHY5nngActivity"]="./../../CoinsWay/deliveryprocesses/resources/Levantar Requisitos_C4A0EA54_b33088a1_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uw,_lwGhUnV1EeiHvoAmdGY1uwActivity"]="./../../CoinsWay/deliveryprocesses/resources/Colocar Funcionalidade em Produção_518C8280_401c20cd_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uwActivity"]="./../../CoinsWay/deliveryprocesses/resources/Semana Ciclo_FDE37A60_c8ae2af9_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uw,_uC28sXWsEeiqgqlHHY5nngActivity"]="./../../CoinsWay/deliveryprocesses/resources/Implementar Funcionalidades_63264BA5_51b5e9f2_Activity.jpeg"
+contentPage.processPage.imageFiles["_U4jVYXV1EeiHvoAmdGY1uw,_cFmK4XV1EeiHvoAmdGY1uw,_tasUAXWtEeiqgqlHHY5nngActivity"]="./../../CoinsWay/deliveryprocesses/resources/Entendimento e Planejamento_684F1753_56deb5a0_Activity.jpeg"
+contentPage.processPage.elementUrls["_g2ZqAHXOEei34J-QrygW6w"] = ["Analisar História de Usuário de um Operador","CoinsWay/deliveryprocesses/analisar_histria_de_usurio_de_um_operador_424313DB.html"];
+contentPage.processPage.elementUrls["_81VSgnWsEeiqgqlHHY5nng"] = ["Operadores","CoinsWay/deliveryprocesses/operadores_6E048009.html"];
+contentPage.processPage.elementUrls["_8gzM8nWsEeiqgqlHHY5nng"] = ["Funcionalidade","CoinsWay/deliveryprocesses/funcionalidade_C043A064.html"];
+contentPage.processPage.elementUrls["_z2ERcHWsEeiqgqlHHY5nng"] = ["Implementar uma Funcionalidade","CoinsWay/deliveryprocesses/implementar_uma_funcionalidade_EE2F77D6.html"];
+contentPage.processPage.elementUrls["_BewfQHWuEeiqgqlHHY5nng"] = ["Detalhar uma Funcionalidade mal entendida","CoinsWay/deliveryprocesses/detalhar_uma_funcionalidade_mal_entendida_33C7FDB7.html"];
+contentPage.processPage.elementUrls["_8gzM8XWsEeiqgqlHHY5nng"] = ["Gerente","CoinsWay/deliveryprocesses/gerente_D1AA8C4E.html"];
+contentPage.processPage.elementUrls["_Dmz4RHWrEeiqgqlHHY5nng"] = ["Todos terão um Entendimento claro do Sistema","CoinsWay/deliveryprocesses/todos_tero_um_entendimento_claro_do_sistema_E8392DC4.html"];
+contentPage.processPage.elementUrls["_8gzM8HWsEeiqgqlHHY5nng"] = ["Teste do Gerente","CoinsWay/deliveryprocesses/teste_do_gerente_C70CAC3E.html"];
+contentPage.processPage.elementUrls["_Dmz4Q3WrEeiqgqlHHY5nng"] = ["Comunicado sobre Funcionalidade em Produção","CoinsWay/deliveryprocesses/comunicado_sobre_funcionalidade_em_produo_85B81590.html"];
+contentPage.processPage.elementUrls["_z2ERdXWsEeiqgqlHHY5nng"] = ["Rascunho de Interface","CoinsWay/deliveryprocesses/rascunho_de_interface_BD5F3A05.html"];
+contentPage.processPage.elementUrls["_BewfQXWuEeiqgqlHHY5nng"] = ["Funcionalidade","CoinsWay/deliveryprocesses/funcionalidade_3E65DDC7.html"];
+contentPage.processPage.elementUrls["__ll-MXWtEeiqgqlHHY5nng"] = ["Administradores","CoinsWay/deliveryprocesses/administradores_A8B3CB86.html"];
+contentPage.processPage.elementUrls["_zUl8EXWsEeiqgqlHHY5nng"] = ["Desenvolvedores","CoinsWay/deliveryprocesses/desenvolvedores_EF823C6.html"];
+contentPage.processPage.elementUrls["_-Q_cZHWlEeiqgqlHHY5nng"] = ["Backup do Banco de Dados","CoinsWay/deliveryprocesses/backup_do_banco_de_dados_CF1B793B.html"];
+contentPage.processPage.elementUrls["_U4jVYXV1EeiHvoAmdGY1uw"] = ["Processo","CoinsWay/deliveryprocesses/Processo_48546145.html"];
+contentPage.processPage.elementUrls["_BewfRHWuEeiqgqlHHY5nng"] = ["Rascunho de Interface","CoinsWay/deliveryprocesses/rascunho_de_interface_F859DFD6.html"];
+contentPage.processPage.elementUrls["_bTPb4nWsEeiqgqlHHY5nng"] = ["Operadores","CoinsWay/deliveryprocesses/operadores_AEFF56AA.html"];
+contentPage.processPage.elementUrls["_-Q_cYXWlEeiqgqlHHY5nng"] = ["Desenvolvedores","CoinsWay/deliveryprocesses/desenvolvedores_1527772C.html"];
+contentPage.processPage.elementUrls["_Dmz4QnWrEeiqgqlHHY5nng"] = ["Bom Relacionamento entre os Stakeholders","CoinsWay/deliveryprocesses/bom_relacionamento_entre_os_stakeholders_1CDE3FCB.html"];
+contentPage.processPage.elementUrls["_81VSgXWsEeiqgqlHHY5nng"] = ["Administradores","CoinsWay/deliveryprocesses/administradores_7F6B6BF3.html"];
+contentPage.processPage.elementUrls["_dniPMXWrEeiqgqlHHY5nng"] = ["Estabilidade","CoinsWay/deliveryprocesses/estabilidade_16316167.html"];
+contentPage.processPage.elementUrls["__ll-M3WtEeiqgqlHHY5nng"] = ["Gerente","CoinsWay/deliveryprocesses/gerente_26B561.html"];
+contentPage.processPage.elementUrls["_BewfRXWuEeiqgqlHHY5nng"] = ["História de Usuário","CoinsWay/deliveryprocesses/historia_de_usuario_2F7BFE6.html"];
+contentPage.processPage.elementUrls["_z2ERcnWsEeiqgqlHHY5nng"] = ["História de Usuário","CoinsWay/deliveryprocesses/historia_de_usuario_E7666BFC.html"];
+contentPage.processPage.elementUrls["_nbmKQHWrEeiqgqlHHY5nng"] = ["Restaurar Backup da Última Versão de Produção","CoinsWay/deliveryprocesses/restaurar_backup_da_verso_da_ltima_verso_de_produo_621C42B4.html"];
+contentPage.processPage.elementUrls["_-Q_cYnWlEeiqgqlHHY5nng"] = ["Funcionalidade","CoinsWay/deliveryprocesses/funcionalidade_3C08B42.html"];
+contentPage.processPage.elementUrls["_zUl8EHWsEeiqgqlHHY5nng"] = ["Escrever e Rodar Testes Unitários","CoinsWay/deliveryprocesses/escrever_e_rodar_testes_unitrios_45A43B6.html"];
+contentPage.processPage.elementUrls["_BewfQ3WuEeiqgqlHHY5nng"] = ["Gravação Explicativo","CoinsWay/deliveryprocesses/gravao_explicativo_95D8C7A2.html"];
+contentPage.processPage.elementUrls["_81VSgHWsEeiqgqlHHY5nng"] = ["Teste do Usuário","CoinsWay/deliveryprocesses/teste_do_usurio_74CD8BE3.html"];
+contentPage.processPage.elementUrls["_cFmK4XV1EeiHvoAmdGY1uw"] = ["Semana (Ciclo)","CoinsWay/deliveryprocesses/Semana Ciclo_FDE37A60.html"];
+contentPage.processPage.elementUrls["__ll-MHWtEeiqgqlHHY5nng"] = ["Reunião Semanal Presencial","CoinsWay/deliveryprocesses/reunio_semanal_presencial_9E15EB76.html"];
+contentPage.processPage.elementUrls["_dniPMHWrEeiqgqlHHY5nng"] = ["Testar Funcionalidade em Produção","CoinsWay/deliveryprocesses/testar_funcionalidade_em_produo_B938157.html"];
+contentPage.processPage.elementUrls["_g2ZqAXXOEei34J-QrygW6w"] = ["Analisar Viabilidade da História de Usuário","CoinsWay/deliveryprocesses/analisar_viabilidade_da_historia_de_usuario_4CE0F3EB.html"];
+contentPage.processPage.elementUrls["_zUl8E3WsEeiqgqlHHY5nng"] = ["Testes Unitários","CoinsWay/deliveryprocesses/testes_unitrios_666B0DA1.html"];
+contentPage.processPage.elementUrls["_BewfQnWuEeiqgqlHHY5nng"] = ["Descrição Detalhada de uma Funcionalidade","CoinsWay/deliveryprocesses/descrio_detalhada_de_uma_funcionalidade_2CFEF1DD.html"];
+contentPage.processPage.elementUrls["_ryJMYHWuEeiqgqlHHY5nng"] = ["Repriorização das Funcionalidades","CoinsWay/deliveryprocesses/repriorizao_das_funcionalidades_C0470007.html"];
+contentPage.processPage.elementUrls["_6olHEXWsEeiqgqlHHY5nng"] = ["Aprovar Implementações","CoinsWay/deliveryprocesses/Aprovar Implementações_75A19CB4.html"];
+contentPage.processPage.elementUrls["_Dmz4QHWrEeiqgqlHHY5nng"] = ["Comunicar que a Funcionalidade está em Produção","CoinsWay/deliveryprocesses/comunicar_que_a_funcionalidade_est_em_produo_23A74BA5.html"];
+contentPage.processPage.elementUrls["_Dmz4QXWrEeiqgqlHHY5nng"] = ["Gerente","CoinsWay/deliveryprocesses/gerente_2E452BB5.html"];
+contentPage.processPage.elementUrls["_bTPb4HWsEeiqgqlHHY5nng"] = ["Escrever História de Usuário","CoinsWay/deliveryprocesses/escrever_histria_de_usurio_B5C86284.html"];
+contentPage.processPage.elementUrls["_g2ZqAnXOEei34J-QrygW6w"] = ["Gerente","CoinsWay/deliveryprocesses/gerente_3B7A0801.html"];
+contentPage.processPage.elementUrls["_WfJCcHWmEeiqgqlHHY5nng"] = ["Subir Nova Funcionalidade para Produção","CoinsWay/deliveryprocesses/subir_nova_funcionalidade_para_produo_7701CA53.html"];
+contentPage.processPage.elementUrls["__ll-MnWtEeiqgqlHHY5nng"] = ["Desenvolvedores","CoinsWay/deliveryprocesses/desenvolvedores_974CDF9C.html"];
+contentPage.processPage.elementUrls["_-Q_cYHWlEeiqgqlHHY5nng"] = ["Backup da Versão de Produção","CoinsWay/deliveryprocesses/backup_da_verso_de_produo_A89971C.html"];
+contentPage.processPage.elementUrls["_tasUAXWtEeiqgqlHHY5nng"] = ["Entendimento e Planejamento","CoinsWay/deliveryprocesses/Entendimento e Planejamento_684F1753.html"];
+contentPage.processPage.elementUrls["_SaCOQHWmEeiqgqlHHY5nng"] = ["Preparar Banco de Produção para Nova Funcionalidade","CoinsWay/deliveryprocesses/preparar_banco_de_produo_para_nova_funcionalidade_9A3BE697.html"];
+contentPage.processPage.elementUrls["_bTPb43WsEeiqgqlHHY5nng"] = ["História de Usuário","CoinsWay/deliveryprocesses/historia_de_usuario_17D92C6F.html"];
+contentPage.processPage.elementUrls["_-Q_cY3WlEeiqgqlHHY5nng"] = ["Backup do Código em Produção","CoinsWay/deliveryprocesses/backup_do_cdigo_em_produo_6C9A6107.html"];
+contentPage.processPage.elementUrls["_z2ERcXWsEeiqgqlHHY5nng"] = ["Gerente","CoinsWay/deliveryprocesses/gerente_F8CD57E6.html"];
+contentPage.processPage.elementUrls["_iwUgMHWmEeiqgqlHHY5nng"] = ["Backup do Banco para poder fazer Testes em Produção","CoinsWay/deliveryprocesses/backup_do_banco_para_testes_em_produo_BDF1BDA1.html"];
+contentPage.processPage.elementUrls["_bTPb4XWsEeiqgqlHHY5nng"] = ["Administradores","CoinsWay/deliveryprocesses/administradores_C0664294.html"];
+contentPage.processPage.elementUrls["_uC28sXWsEeiqgqlHHY5nng"] = ["Implementar Funcionalidades","CoinsWay/deliveryprocesses/Implementar Funcionalidades_63264BA5.html"];
+contentPage.processPage.elementUrls["__ll-NnWtEeiqgqlHHY5nng"] = ["Todos terão um Entendimento claro do Sistema","CoinsWay/deliveryprocesses/todos_tero_um_entendimento_claro_do_sistema_5BDEC1BB.html"];
+contentPage.processPage.elementUrls["_g2ZqA3XOEei34J-QrygW6w"] = ["Desenvolvedores","CoinsWay/deliveryprocesses/desenvolvedores_A453DDC6.html"];
+contentPage.processPage.elementUrls["_CafPEHWrEeiqgqlHHY5nng"] = ["Restaurar Backup do Banco feito Antes dos Testes","CoinsWay/deliveryprocesses/restaurar_backup_do_banco_feito_antes_dos_testes_351D6736.html"];
+contentPage.processPage.elementUrls["__ll-NHWtEeiqgqlHHY5nng"] = ["Operadores","CoinsWay/deliveryprocesses/operadores_62A7CD95.html"];
+contentPage.processPage.elementUrls["_CafPEXWrEeiqgqlHHY5nng"] = ["Usuários Pouparão Tempo Antes desperdiçado fora do Sistema","CoinsWay/deliveryprocesses/usurios_pouparo_tempo_antes_desperdiado_fora_do_sistema_3FBB4746.html"];
+contentPage.processPage.elementUrls["_z2ERdHWsEeiqgqlHHY5nng"] = ["Gravação Explicativo","CoinsWay/deliveryprocesses/gravao_explicativo_B2C159F5.html"];
+contentPage.processPage.elementUrls["_Sr6ZsXWsEeiqgqlHHY5nng"] = ["Definir Funcionalidades (Levantar Requisitos)","CoinsWay/deliveryprocesses/Levantar Requisitos_C4A0EA54.html"];
+contentPage.processPage.elementUrls["_z2ERc3WsEeiqgqlHHY5nng"] = ["Descrição Detalhada de uma Funcionalidade","CoinsWay/deliveryprocesses/descrio_detalhada_de_uma_funcionalidade_504041C1.html"];
+contentPage.processPage.elementUrls["_lwGhUnV1EeiHvoAmdGY1uw"] = ["Colocar Funcionalidade em Produção","CoinsWay/deliveryprocesses/Colocar Funcionalidade em Produção_518C8280.html"];
+contentPage.processPage.elementUrls["__ll-NXWtEeiqgqlHHY5nng"] = ["Bom Relacionamento entre os Stakeholders","CoinsWay/deliveryprocesses/bom_relacionamento_entre_os_stakeholders_6D45ADA5.html"];
+contentPage.processPage.elementUrls["_zUl8EnWsEeiqgqlHHY5nng"] = ["Funcionalidade","CoinsWay/deliveryprocesses/funcionalidade_FD9137DC.html"];
