e7eb0461883876b087d5bbd434cf0c42c0902214
Pedro Duarte <coinbr@gmail.com>
2018-06-28 16:25:33 -0300

    Add tmp Max Marilia EPF Project

---
+++ tmp/max/scripts/processElementData.js
@@ -0,0 +1,26 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_81LV0XmvEeijjfO68QzPag,_kgVVEXmzEeijjfO68QzPagActivity"]="./../../FMCPA/deliveryprocesses/resources/Avaliação_ECD48994_45c79154_Activity.jpeg"
+contentPage.processPage.imageFiles["_81LV0XmvEeijjfO68QzPagActivity"]="./../../FMCPA/deliveryprocesses/resources/CicloDeVidaFMCPA_E434FD6C_e434fd6c_Activity.jpeg"
+contentPage.processPage.imageFiles["_81LV0XmvEeijjfO68QzPag,_mU_JkXmzEeijjfO68QzPagActivity"]="./../../FMCPA/deliveryprocesses/resources/Melhoria_C57A40A5_1e6d4865_Activity.jpeg"
+contentPage.processPage.elementUrls["_nNCy8XmzEeijjfO68QzPag"] = ["1. Planejamneto De Avaliação","FMCPA/deliveryprocesses/1. Planejamneto De Avaliação_3755B89B.html"];
+contentPage.processPage.elementUrls["_4K_h0XmzEeijjfO68QzPag"] = ["6. Execução","FMCPA/deliveryprocesses/6. Execução_201273AD.html"];
+contentPage.processPage.elementUrls["_mU_JkXmzEeijjfO68QzPag"] = ["Melhoria","FMCPA/deliveryprocesses/Melhoria_C57A40A5.html"];
+contentPage.processPage.elementUrls["_81LV0XmvEeijjfO68QzPag"] = ["CicloDeVidaFMCPA","FMCPA/deliveryprocesses/CicloDeVidaFMCPA_E434FD6C.html"];
+contentPage.processPage.elementUrls["_vUwg8XmzEeijjfO68QzPag"] = ["3. Análise Dos Resultados","FMCPA/deliveryprocesses/3. Análise Dos Resultados_5A96EFD4.html"];
+contentPage.processPage.elementUrls["_kgVVEXmzEeijjfO68QzPag"] = ["Avaliação","FMCPA/deliveryprocesses/Avaliação_ECD48994.html"];
+contentPage.processPage.elementUrls["_zDzlkXmzEeijjfO68QzPag"] = ["4. Planejamento Da Melhoria","FMCPA/deliveryprocesses/4. Planejamento Da Melhoria_82CF04CE.html"];
+contentPage.processPage.elementUrls["_quxNQXmzEeijjfO68QzPag"] = ["2. Execução Da Avaliação","FMCPA/deliveryprocesses/2. Execução Da Avaliação_51565A66.html"];
+contentPage.processPage.elementUrls["_6G0VUXmzEeijjfO68QzPag"] = ["7. Revisão","FMCPA/deliveryprocesses/7. Revisão_671E865F.html"];
+contentPage.processPage.elementUrls["_2qkQ4XmzEeijjfO68QzPag"] = ["5. Inclusão","FMCPA/deliveryprocesses/5. Inclusão_654A3C2E.html"];
