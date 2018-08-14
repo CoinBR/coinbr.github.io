6593b33dc30f01bbf3958d82ad1ef003dafeee69
Pedro Duarte <coinbr@gmail.com>
2018-08-14 19:54:14 -0300

    VisualThinker: filter and CamelCase words

--- tmp/visual-thinker/Word Cloud/js/get-words.js
+++ tmp/visual-thinker/Word Cloud/js/get-words.js
@@ -2,18 +2,39 @@ $(document).ready(function(){
 	
 	document.getElementById("genCloud").onclick = (function()
 	{
-		
+
+		const EXCLUDED_WORDS = ["a", "ao", "e", "é", "em", "o", "ó", "ou", "da", "de", "do", "na", "nem", "no", "nós", "mim", "se", "ser", "que",
+					"são", "pra", "por", "pro", "para", "pois", "eu", "você", "vocês", "vou", "tu", "um", "uma", "uns", "umas", "como",
+					"ele", "ela", "eles", "elas", "através", "todo", "aqui", "muito", "pouco", "está", "estão", "esta", "sendo", "tendo",
+					"além", "possam", "podem", "nesse", "nessa", "neste", "nesta", "ficar", "fica", "ficarem", "permite", "permitem",
+					"seja", "sejam", "sejamos", "acontece", "acontecem", "acontecerão", "aconteceram", "mostrar", "mostra", "mostrando",
+					"amostrado", "mesmo", "determina", "determinado", "determinada", "apenas", "espero", "esperava", "esperando", "espera",
+					"salve", "lhe", "lá", "lo", "momento", "momentos", "momentâneo", "momentâneamente", "dar", "dou", "deu", "dão", "dói",
+					"faço", "faça", "façam", "façamos", "farei", "feito"];
+
+		function camelizeWords(words){
+			return words.map(function(word){
+				const lowered = word.toLowerCase();
+				return word.substring(0, 1).toUpperCase() + lowered.substring(1,999);
+			});
+		}
+
+		const CAMELIZED_EXCLUDED_WORDS = camelizeWords(EXCLUDED_WORDS); 
+
+		function filterWords(words){
+			return words.filter(word => !CAMELIZED_EXCLUDED_WORDS.includes(word));
+		};
+
 		function getWords(){
 			var txt = document.getElementById("answers").value;
 			txt.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); // remove ponctuation
 			txt.replace(/(\r\n\t|\n|\r\t)/gm,""); // remove line breaks
 			
 			var words = txt.split(" ");
-			return words;
+			const camelized = camelizeWords(words);
+			return filterWords(camelized);
 		}
 		
-		
-
 		
 		function createWeightedWordsArray(raw, words){
 			if (raw === undefined || raw.length == 0){
